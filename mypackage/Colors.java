53ebcc5d0c565b97f50769634e2b91021e62d5f0
Cyprien Ferreira <cyferrei@f2r2s2.clusters.42paris.fr>
2025-09-29 14:22:36 +0200

    [ADD]: add package folder

---
+++ mypackage/Colors.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class Colors {
+
+	public static final String RED_BOLD = "\033[1;31m";
+	public static final String GREEN_BOLD = "\033[1;32m";
+	public static final String ORANGE_BOLD = "\033[1;38;5;208m";
+	public static final String ITALIC = "\033[3m";
+	public static final String RESET = "\033[0m";
+}
