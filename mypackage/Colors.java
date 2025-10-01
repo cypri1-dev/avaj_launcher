38634da19ba5bdbebcb2e417b567b7043bbdc0d3
cyferrei <cyferrei@student.42.fr>
2025-10-01 11:11:20 +0200

    [ADD]: add debug statement

--- mypackage/Colors.java
+++ mypackage/Colors.java
@@ -5,6 +5,7 @@ public class Colors {
 	public static final String RED_BOLD = "\033[1;31m";
 	public static final String GREEN_BOLD = "\033[1;32m";
 	public static final String ORANGE_BOLD = "\033[1;38;5;208m";
+	public static final String DEBUG_BOLD = "\u001B[1;34m" + "[DEBUG]: " + "\033[0m";
 	public static final String ITALIC = "\033[3m";
 	public static final String RESET = "\033[0m";
 }
