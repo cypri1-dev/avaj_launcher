aa235082e6d861c424d3231e2a1ca464f6b5aafb
cyferrei <cyferrei@student.42.fr>
2025-10-01 15:08:59 +0200

    [ADD]: add tests to check creation a new objects

--- sources/Simulator.java
+++ sources/Simulator.java
@@ -27,6 +27,17 @@ public class Simulator {
 			return;
 		if (!Extractor.extractData(data, sim))
 			return;
-		System.out.println(DEBUG_BOLD + "sim.nbSimulation: " + sim.getNbSimulation());
+		// System.out.println(DEBUG_BOLD + "sim.nbSimulation: " + sim.getNbSimulation());
+
+		Coordinates coorH1 = new Coordinates(23, 12, 43);
+		Helicopter H1 = new Helicopter(1234, "testHelico", coorH1);
+
+		System.out.println(DEBUG_BOLD + "HelicoTest");
+		System.out.println(DEBUG_BOLD + "id: " + H1.id);
+		System.out.println(DEBUG_BOLD + "name: " + H1.name);
+		System.out.println(DEBUG_BOLD + "longitude: " + H1.coordinates.getLongitude());
+		System.out.println(DEBUG_BOLD + "latitude: " + H1.coordinates.getLatitude());
+		System.out.println(DEBUG_BOLD + "height: " + H1.coordinates.getHeight());
+
 	}
 }
