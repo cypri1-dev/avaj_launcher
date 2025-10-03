2f69df8b77993722a447f32dc731977092e47679
cyferrei <cyferrei@student.42.fr>
2025-10-03 16:49:19 +0200

    [ADD]: add new class Tower

---
+++ sources/Tower.java
@@ -0,0 +1,36 @@
+package sources;
+
+import static mypackage.Colors.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Tower {
+
+	private List<Flyable> observers = new ArrayList<>();
+	final private String registerMsg = " registered to waether tower.";
+
+	public void register(Flyable p_flyable) {
+		
+		String formatName = p_flyable.getType() + "#" + p_flyable.getName() + "(" + p_flyable.getId() + ")";
+		System.out.println(DEBUG_BOLD + "foramtName: " + RESET + formatName);
+		observers.add(p_flyable);
+		System.out.println(GREEN_BOLD + "Tower says" + RESET + ": " + formatName + registerMsg);
+	}
+
+	public void unregister(Flyable p_flyable) {
+		// logic here
+	}
+
+	protected void conditionChanged() {
+		// logic here
+	}
+
+	/* PRINTING FUNCTION FOR DEBUG*/
+
+	public void printListRegisteredFlyable() {
+
+		for (Flyable aircraft : this.observers) {
+			System.out.println("Aircraft: " + aircraft.getName());
+		}
+	}
+}
