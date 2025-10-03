9152ed2b3ff597ebf8801fa3ebd46377f5f9adc3
cyferrei <cyferrei@student.42.fr>
2025-10-03 16:50:38 +0200

    [ADD]: add new attribute type - also add getter to reach data from Flayble class

--- sources/Aircraft.java
+++ sources/Aircraft.java
@@ -6,14 +6,28 @@ public class Aircraft extends Flyable {
 	protected String name;
 	protected Coordinates coordinates;
 
+	// need a type attribute - also need getter for it
+	protected String type;
+
 	protected Aircraft(long p_id, String p_name, Coordinates p_coodinates) {
 		this.id = p_id;
 		this.name = p_name;
 		this.coordinates = p_coodinates;
+		this.type = "defaultType";
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public long getId() {
+		return this.id;
 	}
 
 	/* Must be declared here but empty - will be overrided in children */
 	public void updateConditions() {}
-
-	/* May add method to create unique id */
 }
