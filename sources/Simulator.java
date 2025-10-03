90080a7d1d68fa48340c3703663ac2c1e180c1e5
cyferrei <cyferrei@student.42.fr>
2025-10-03 16:55:50 +0200

    [MODIF/ADD]: new tests

--- sources/Simulator.java
+++ sources/Simulator.java
@@ -21,21 +21,43 @@ public class Simulator {
 			return;
 
 		Simulator sim = new Simulator();
+		Tower controlTower = new Tower();
 		data = Parser.readFile(args[0]);
 		if (data == null)
 			return;
-		if (!Extractor.extractData(data, sim))
+		if (!Extractor.extractData(data, sim, controlTower))
 			return;
+			
+		/* -------------------------------- TESTS PART -------------------------------- */
+
 		// System.out.println(DEBUG_BOLD + "sim.nbSimulation: " + sim.getNbSimulation());
+			
+		// Coordinates coor = new Coordinates(23, 12, 43);
+		// Helicopter H1 = new Helicopter(1234, "testHelico", coor);
+		// JetPlane J1 = new JetPlane(12345, "testJetPlane", coor);
+		// Baloon B1 = new Baloon(12346, "testBaloon", coor);
 
-		Coordinates coorH1 = new Coordinates(23, 12, 43);
-		Helicopter H1 = new Helicopter(1234, "testHelico", coorH1);
+		// System.out.println(DEBUG_BOLD + "HelicoTest");
+		// System.out.println(DEBUG_BOLD + "id: " + H1.id);
+		// System.out.println(DEBUG_BOLD + "name: " + H1.name);
+		// System.out.println(DEBUG_BOLD + "longitude: " + H1.coordinates.getLongitude());
+		// System.out.println(DEBUG_BOLD + "latitude: " + H1.coordinates.getLatitude());
+		// System.out.println(DEBUG_BOLD + "height: " + H1.coordinates.getHeight());
 
-		System.out.println(DEBUG_BOLD + "HelicoTest");
-		System.out.println(DEBUG_BOLD + "id: " + H1.id);
-		System.out.println(DEBUG_BOLD + "name: " + H1.name);
-		System.out.println(DEBUG_BOLD + "longitude: " + H1.coordinates.getLongitude());
-		System.out.println(DEBUG_BOLD + "latitude: " + H1.coordinates.getLatitude());
-		System.out.println(DEBUG_BOLD + "height: " + H1.coordinates.getHeight());
+		// System.out.println(DEBUG_BOLD + "JetPlaneTest");
+		// System.out.println(DEBUG_BOLD + "id: " + J1.id);
+		// System.out.println(DEBUG_BOLD + "name: " + J1.name);
+		// System.out.println(DEBUG_BOLD + "longitude: " + J1.coordinates.getLongitude());
+		// System.out.println(DEBUG_BOLD + "latitude: " + J1.coordinates.getLatitude());
+		// System.out.println(DEBUG_BOLD + "height: " + J1.coordinates.getHeight());
+
+		// System.out.println(DEBUG_BOLD + "BaloonTest");
+		// System.out.println(DEBUG_BOLD + "id: " + B1.id);
+		// System.out.println(DEBUG_BOLD + "name: " + B1.name);
+		// System.out.println(DEBUG_BOLD + "longitude: " + B1.coordinates.getLongitude());
+		// System.out.println(DEBUG_BOLD + "latitude: " + B1.coordinates.getLatitude());
+		// System.out.println(DEBUG_BOLD + "height: " + B1.coordinates.getHeight());
+		
+		controlTower.printListRegisteredFlyable();
 	}
 }
