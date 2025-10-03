aa6f5b12eb808c74f31c8615b9597e520dfb4e69
cyferrei <cyferrei@student.42.fr>
2025-10-03 16:51:39 +0200

    [ADD]: add type attribute in constructor

--- sources/JetPlane.java
+++ sources/JetPlane.java
@@ -4,6 +4,7 @@ public class JetPlane extends Aircraft {
 
 	public JetPlane(long p_id, String p_name, Coordinates p_coordinate) {
 		super(p_id, p_name, p_coordinate);
+		this.type = "JetPlane";
 	}
 
 	@Override
