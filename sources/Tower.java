842a5ccbee3428fb3cad4775f214dcc32f0074a7
cyferrei <cyferrei@student.42.fr>
2025-10-05 21:24:19 +0200

    [MODIF/ADD]: little modif for log msg - add method conditionChanged

--- sources/Tower.java
+++ sources/Tower.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 public class Tower {
 
 	private List<Flyable> observers = new ArrayList<>();
-	final private String registerMsg = " registered to waether tower.";
+	final private String registerMsg = " registered to weather tower.";
 
 	public void register(Flyable p_flyable) {
 		
 		String formatName = p_flyable.getType() + "#" + p_flyable.getName() + "(" + p_flyable.getId() + ")";
-		System.out.println(DEBUG_BOLD + "foramtName: " + RESET + formatName);
+		// System.out.println(DEBUG_BOLD + "foramtName: " + RESET + formatName);
 		observers.add(p_flyable);
 		System.out.println(GREEN_BOLD + "Tower says" + RESET + ": " + formatName + registerMsg);
 	}
@@ -22,7 +22,9 @@ public class Tower {
 	}
 
 	protected void conditionChanged() {
-		// logic here
+		for (Flyable registedAircraft : this.observers) {
+			registedAircraft.updateConditions();
+		}
 	}
 
 	/* PRINTING FUNCTION FOR DEBUG*/
