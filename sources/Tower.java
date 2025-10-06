68e741cde7391f66c23eef126ab9727ecc080660
cyferrei <cyferrei@student.42.fr>
2025-10-06 18:39:10 +0200

    [MODIF]: modif register and unregister method to print in file

--- sources/Tower.java
+++ sources/Tower.java
@@ -3,6 +3,9 @@ package sources;
 import static mypackage.Colors.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Tower {
 
@@ -15,14 +18,31 @@ public class Tower {
 		String formatName = p_flyable.getType() + "#" + p_flyable.getName() + "(" + p_flyable.getId() + ")";
 		// System.out.println(DEBUG_BOLD + "foramtName: " + RESET + formatName);
 		observers.add(p_flyable);
-		System.out.println(GREEN_BOLD + "Tower says" + RESET + ": " + formatName + registerMsg);
+		String txt = "Tower says: " + formatName + registerMsg;
+		String fileOut = "simulation.txt";
+
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileOut, true))) {
+			writer.write(txt);
+			writer.newLine();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void unregister(Flyable p_flyable) {
 		String formatName = p_flyable.getType() + "#" + p_flyable.getName() + "(" + p_flyable.getId() + ")";
 		observers.remove(p_flyable);
-		System.out.println(GREEN_BOLD + "Tower says" + RESET + ": " + formatName + unregisterMsg);
-		// logic here
+		String txt = "Tower says: " + formatName + unregisterMsg;
+		String fileOut = "simulation.txt";
+
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileOut, true))) {
+			writer.write(txt);
+			writer.newLine();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 protected void conditionChanged() {
