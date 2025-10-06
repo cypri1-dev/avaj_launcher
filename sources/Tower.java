a9393362d8de384f5dd323395f4ff15555dfef4c
cyferrei <cyferrei@student.42.fr>
2025-10-06 17:35:39 +0200

    [MODIF]: modif method conditionChanged()

--- sources/Tower.java
+++ sources/Tower.java
@@ -8,6 +8,7 @@ public class Tower {
 
 	private List<Flyable> observers = new ArrayList<>();
 	final private String registerMsg = " registered to weather tower.";
+	final private String unregisterMsg = " unregistered from weather tower.";
 
 	public void register(Flyable p_flyable) {
 		
@@ -18,14 +19,21 @@ public class Tower {
 	}
 
 	public void unregister(Flyable p_flyable) {
+		String formatName = p_flyable.getType() + "#" + p_flyable.getName() + "(" + p_flyable.getId() + ")";
+		observers.remove(p_flyable);
+		System.out.println(GREEN_BOLD + "Tower says" + RESET + ": " + formatName + unregisterMsg);
 		// logic here
 	}
 
-	protected void conditionChanged() {
-		for (Flyable registedAircraft : this.observers) {
-			registedAircraft.updateConditions();
-		}
+protected void conditionChanged() {
+	// need copy !
+	List<Flyable> copyObservers = new ArrayList<>(this.observers);
+
+	for (Flyable registeredAircraft : copyObservers) {
+		registeredAircraft.updateConditions();
 	}
+}
+
 
 	/* PRINTING FUNCTION FOR DEBUG*/
 
