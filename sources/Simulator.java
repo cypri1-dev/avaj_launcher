953bb41a3a4f12003651c6ec1220eb2df225e66d
cyferrei <cyferrei@student.42.fr>
2025-10-06 18:40:14 +0200

    [UPDATE]: simulator is now OK - may make some clean

--- sources/Simulator.java
+++ sources/Simulator.java
@@ -44,40 +44,7 @@ public class Simulator {
 
 		for (int i = 0; i < sim.getNbSimulation(); ++i) {
 			weatherTower.changeWeather();
-			System.out.println("------------------------------------------------------------------------------------");
+			// System.out.println("------------------------------------------------------------------------------------");
 		}
-
-		/* -------------------------------- TESTS PART -------------------------------- */
-
-		System.out.println(DEBUG_BOLD + "sim.nbSimulation: " + sim.getNbSimulation());
-			
-		// Coordinates coor = new Coordinates(23, 12, 43);
-		// Helicopter H1 = new Helicopter(1234, "testHelico", coor);
-		// JetPlane J1 = new JetPlane(12345, "testJetPlane", coor);
-		// Baloon B1 = new Baloon(12346, "testBaloon", coor);
-
-		// System.out.println(DEBUG_BOLD + "HelicoTest");
-		// System.out.println(DEBUG_BOLD + "id: " + H1.id);
-		// System.out.println(DEBUG_BOLD + "name: " + H1.name);
-		// System.out.println(DEBUG_BOLD + "longitude: " + H1.coordinates.getLongitude());
-		// System.out.println(DEBUG_BOLD + "latitude: " + H1.coordinates.getLatitude());
-		// System.out.println(DEBUG_BOLD + "height: " + H1.coordinates.getHeight());
-
-		// System.out.println(DEBUG_BOLD + "JetPlaneTest");
-		// System.out.println(DEBUG_BOLD + "id: " + J1.id);
-		// System.out.println(DEBUG_BOLD + "name: " + J1.name);
-		// System.out.println(DEBUG_BOLD + "longitude: " + J1.coordinates.getLongitude());
-		// System.out.println(DEBUG_BOLD + "latitude: " + J1.coordinates.getLatitude());
-		// System.out.println(DEBUG_BOLD + "height: " + J1.coordinates.getHeight());
-
-		// System.out.println(DEBUG_BOLD + "BaloonTest");
-		// System.out.println(DEBUG_BOLD + "id: " + B1.id);
-		// System.out.println(DEBUG_BOLD + "name: " + B1.name);
-		// System.out.println(DEBUG_BOLD + "longitude: " + B1.coordinates.getLongitude());
-		// System.out.println(DEBUG_BOLD + "latitude: " + B1.coordinates.getLatitude());
-		// System.out.println(DEBUG_BOLD + "height: " + B1.coordinates.getHeight());
-		
-		// controlTower.printListRegisteredFlyable();
-		// WeatherProvider.getInstance().getCurrentWeather(coor);
 	}
 }
